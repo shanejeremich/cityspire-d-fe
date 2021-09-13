f886a0bcaab436da7bf01c63244ff08b0d23b181
Shane Jeremich <shane.jeremich@gmail.com>
2021-09-13 02:04:56 -0600

    testing config

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,12 +1,15 @@
 import { CALLBACK_PATH } from '../api/CONSTANTS';
 
 const REDIRECT_URI = `${window.location.origin}${CALLBACK_PATH}`;
+const OKTA_TESTING_DISABLEHTTPSCHECK =
+  process.env.OKTA_TESTING_DISABLEHTTPSCHECK || false;
 
 const config = {
   issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
   redirectUri: REDIRECT_URI,
   clientId: process.env.REACT_APP_CLIENT_ID,
   pkce: true,
+  disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK,
   scopes: ['openid', 'email', 'profile'],
 };
 
