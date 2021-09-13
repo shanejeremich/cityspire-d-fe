3f003bb85bd24f016be5089ca98567811a61d405
Shane Jeremich <shane.jeremich@gmail.com>
2021-09-13 01:24:30 -0600

    testing config

--- src/utils/oktaConfig.js
+++ src/utils/oktaConfig.js
@@ -1,6 +1,8 @@
+const REDIRECT_URI = `${window.location.origin}/cityspire-d-fe/login/callback`;
+
 const config = {
   issuer: process.env.REACT_APP_OKTA_ISSUER_URI,
-  redirectUri: window.location.origin + '/cityspire-d-fe/implicit/callback',
+  redirectUri: REDIRECT_URI,
   clientId: process.env.REACT_APP_CLIENT_ID,
   pkce: true,
   scopes: ['openid', 'email', 'profile'],
