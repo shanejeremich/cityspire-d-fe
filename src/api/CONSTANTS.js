e3db5946ff37e1492d5068a0c02caf15e6e938cb
Shane Jeremich <shane.jeremich@gmail.com>
2021-09-16 17:46:51 -0600

    updated paths in api/constants

--- src/api/CONSTANTS.js
+++ src/api/CONSTANTS.js
@@ -2,15 +2,15 @@
  * PATHS
  */
 // prettier-ignore
-export const ROOT = '/cityspire-d-fe',
-CALLBACK_PATH = '/cityspire-d-fe/implicit/callback',
-LOCATIONS = '/cityspire-d-fe/locations',
-USER_LOCATIONS = '/cityspire-d-fe/userlocations/',
-PROFILES = '/cityspire-d-fe/profiles',
-LOGIN = '/cityspire-d-fe/login',
-MAP = '/cityspire-d-fe/map',
-HOME = '/cityspire-d-fe/home',
-EXAMPLE_LIST = '/cityspire-d-fe/example-list',
-FAVORITES = '/cityspire-d-fe/favorites',
-PROFILE_LIST = '/cityspire-d-fe/profile-list',
-DATA_VIS = '/cityspire-d-fe/datavis';
+export const ROOT = '/',
+CALLBACK_PATH = '/implicit/callback',
+LOCATIONS = '/locations',
+USER_LOCATIONS = '/userlocations/',
+PROFILES = '/profiles',
+LOGIN = '/login',
+MAP = '/map',
+HOME = '/home',
+EXAMPLE_LIST = '/example-list',
+FAVORITES = '/favorites',
+PROFILE_LIST = '/profile-list',
+DATA_VIS = '/datavis';
