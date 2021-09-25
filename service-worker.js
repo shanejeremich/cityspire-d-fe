88c84fe55ce414c0af3547d2b3c193ba07e5c735
Shane Jeremich <shane.jeremich@gmail.com>
2021-09-25 14:46:09 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.86bd349972acc20062c3fe00f58a75f7.js"
+  "/precache-manifest.55bc376998cc6b328f3d0c9e7a47bcec.js"
 );
 
 self.addEventListener('message', (event) => {
